28b8ed1b1b8b0cbbea0e178717d1900a54066655
praful99665 <praful99665@gmail.com>
2025-02-16 20:03:25 +0530

    Initial commit for student_management app

---
+++ student_management/student_management/doctype/courses/courses.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, praful and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Courses(Document):
+	pass
---
+++ student_management/student_management/doctype/student_enrollment/student_enrollment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, praful and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Student Enrollment", {
+// 	refresh(frm) {
+
+// 	},
+// });
